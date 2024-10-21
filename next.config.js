ed58bc17bae396a17b44ec27a946f1844c65d213
cblanco-clintell <c.blanco@clintell.io>
2024-10-21 13:01:22 +0200

    Fix - Type Error in Builds

--- next.config.js
+++ next.config.js
@@ -4,13 +4,14 @@ const nextConfig = {
     NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
   },
   typescript: {
-    ignoreBuildErrors: process.env.NODE_ENV !== 'production',
+    ignoreBuildErrors: process.env.NODE_ENV !== 'production' || process.env.ENVIRONMENT === 'local',
   },
 };
 
-if (process.env.NODE_ENV === 'development' || process.env.DEVELOPMENT === 'true') {
-  console.log('Running in development mode');
+if (process.env.NODE_ENV === 'development' || process.env.DEVELOPMENT === 'true' || process.env.ENVIRONMENT === 'local') {
+  console.log('Running in development or local mode');
   console.log('NEXT_PUBLIC_HOST:', process.env.NEXT_PUBLIC_HOST);
+  console.log('ENVIRONMENT:', process.env.ENVIRONMENT);
 }
 
 module.exports = nextConfig;
